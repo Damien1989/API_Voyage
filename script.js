215d8ccc59d25b2be2d55ac24c1e323ec134b7d8
damien <contact.damiendiaz@gmail.com>
2023-12-17 14:00:14 +0100

    infos trip

--- script.js
+++ script.js
@@ -6,3 +6,31 @@ const pays = document.getElementById('pays');
 const start = document.getElementById('start');
 const end = document.getElementById('end');
 const liste = document.querySelector('.liste');
+
+const voyages = [
+    {
+        pays : "La Réunion",
+        prix : 1790,
+        voyageurs : 4
+    },
+    {
+        pays : "Martinique",
+        prix : 2290,
+        voyageurs : 4
+    },
+    {
+        pays : "Tahiti",
+        prix : 1490,
+        voyageurs : 3
+    },
+    {
+        pays : "Guyane",
+        prix : 1790,
+        voyageurs : 4
+    },
+    {
+        pays : "Philippines",
+        prix : 1390,
+        voyageurs : 4
+    },
+]
