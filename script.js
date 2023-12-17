4189e773f340b324fb0e29b9c6224f24f07a9b4f
damien <contact.damiendiaz@gmail.com>
2023-12-17 13:41:38 +0100

    script

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log('connecté !');
+
+const form = document.getElementById('form');
+
+const pays = document.getElementById('pays');
+const start = document.getElementById('start');
+const end = document.getElementById('end');
+const liste = document.querySelector('.liste');
